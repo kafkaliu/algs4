ccc2f033b72f351573e534499967e0ddfb8cacf2
Kafka Liu <kafka.liu@gmail.com>
2014-02-09 18:00:27 +0800

    first commit

---
+++ percolation/src/Percolation.java
@@ -0,0 +1,22 @@
+/**
+ * Created by Kafka Liu on 14-2-9.
+ */
+public class Percolation {
+  public Percolation(int N) {
+    throw new IllegalArgumentException("Not implemented.");
+  }             // create N-by-N grid, with all sites blocked
+
+  public void open(int i, int j) {
+
+  }         // open site (row i, column j) if it is not already
+
+  public boolean isOpen(int i, int j) {
+    throw new IllegalArgumentException("Not implemented.");
+  }    // is site (row i, column j) open?
+  public boolean isFull(int i, int j) {
+    throw new IllegalArgumentException("Not implemented.");
+  }   // is site (row i, column j) full?
+  public boolean percolates() {
+    throw new IllegalArgumentException("Not implemented.");
+  }           // does the system percolate?
+}
---
+++ percolation/src/PercolationStats.java
@@ -0,0 +1,23 @@
+/**
+ * Created by Kafka Liu on 14-2-9.
+ */
+public class PercolationStats {
+  public PercolationStats(int N, int T) {
+    throw new IllegalArgumentException("Not implemented.");
+  }   // perform T independent computational experiments on an N-by-N grid
+  public double mean() {
+    throw new IllegalArgumentException("Not implemented.");
+  }                    // sample mean of percolation threshold
+  public double stddev() {
+    throw new IllegalArgumentException("Not implemented.");
+  }                  // sample standard deviation of percolation threshold
+  public double confidenceLo() {
+    throw new IllegalArgumentException("Not implemented.");
+  }            // returns lower bound of the 95% confidence interval
+  public double confidenceHi() {
+    throw new IllegalArgumentException("Not implemented.");
+  }            // returns upper bound of the 95% confidence interval
+  public static void main(String[] args) {
+    throw new IllegalArgumentException("Not implemented.");
+  }  // test client, described below
+}
